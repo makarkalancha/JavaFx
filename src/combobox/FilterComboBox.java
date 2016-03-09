6a73a5eb8c89e9c28c28e78df8cc665004292802
makarkalancha@gmail.com <makarkalancha@gmail.com>
2016-03-09 12:40:18 -0500

    initial

---
+++ src/combobox/FilterComboBox.java
@@ -0,0 +1,110 @@
+package combobox;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.apache.commons.lang.StringUtils;
+import javafx.application.Platform;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
+/**
+ * Created by mcalancea on 2016-03-09.
+ */
+
+public class FilterComboBox extends ComboBox<String> {
+
+    private ObservableList<String> initialList;
+    private ObservableList<String> bufferList = FXCollections.observableArrayList();
+    private String previousValue = "";
+
+    public FilterComboBox() {
+
+    }
+
+    public FilterComboBox(ObservableList<String> items) {
+        super(items);
+        super.setEditable(true);
+        this.initialList = items;
+
+        this.configAutoFilterListener();
+    }
+
+    private void configAutoFilterListener() {
+        final FilterComboBox currentInstance = this;
+        this.getEditor().textProperty().addListener(new ChangeListener<String>() {
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                previousValue = oldValue;
+                final TextField editor = currentInstance.getEditor();
+                final String selected = currentInstance.getSelectionModel().getSelectedItem();
+
+                if (selected == null || !selected.equals(editor.getText())) {
+                    filterItems(newValue, currentInstance);
+
+                    currentInstance.show();
+//                    if (currentInstance.getItems().size() == 1) {
+//                        setUserInputToOnlyOption(currentInstance, editor);
+//                    }
+                }
+            }
+        });
+    }
+
+    private void filterItems(String filter, ComboBox<String> comboBox) {
+        if(StringUtils.isEmpty(filter)){
+            bufferList = this.readFromList(filter, initialList);
+        }else {
+            StringBuilder regex = new StringBuilder();
+            for (int i = 0; i < filter.length(); i++) {
+                regex.append(filter.charAt(i));
+                regex.append(".*");
+            }
+            bufferList.clear();
+            Pattern pattern = Pattern.compile(regex.toString(), Pattern.CASE_INSENSITIVE);
+            for (String string : initialList) {
+                Matcher matcher = pattern.matcher(string);
+                if (matcher.find()) {
+                    bufferList.add(string);
+                }
+            }
+        }
+
+//        if (filter.startsWith(previousValue) && !previousValue.isEmpty()) {
+//            ObservableList<String> filteredList = this.readFromList(filter, bufferList);
+//            bufferList.clear();
+//            bufferList = filteredList;
+//        } else {
+//            bufferList = this.readFromList(filter, initialList);
+//        }
+        comboBox.setItems(bufferList);
+    }
+
+    private ObservableList<String> readFromList(String filter, ObservableList<String> originalList) {
+        ObservableList<String> filteredList = FXCollections.observableArrayList();
+        for (String item : originalList) {
+            if (item.toLowerCase().startsWith(filter.toLowerCase())) {
+                filteredList.add(item);
+            }
+        }
+
+        return filteredList;
+    }
+
+    private void setUserInputToOnlyOption(ComboBox<String> currentInstance, final TextField editor) {
+        final String onlyOption = currentInstance.getItems().get(0);
+        final String currentText = editor.getText();
+        if (onlyOption.length() > currentText.length()) {
+            editor.setText(onlyOption);
+            Platform.runLater(new Runnable() {
+                @Override
+                public void run() {
+                    editor.selectRange(currentText.length(), onlyOption.length());
+                }
+            });
+        }
+    }
+}
